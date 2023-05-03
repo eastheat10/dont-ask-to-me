5e1554ab3f38f6f17c6035c1f4a059bbadbd5543
Jay <bjw1622@gmail.com>
2023-05-03 10:39:41 +0900

    feat : postRepository 추가

--- src/firebase/PostRepository.js
+++ src/firebase/PostRepository.js
@@ -8,6 +8,26 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 const questions = "questions";
 
-export const findPosts = () => {
-  return getCollections(questions);
+export const writeQuestion = (content, author) => {
+  write(questions, {
+    content: content,
+    author: author,
+    createdAt: Date.now(),
+  });
+};
+
+let lastTime = 9999999999999;
+
+export const readPagedQuestion = async () => {
+  const q = query(collection(db, questions), orderBy("n", "desc"), limit(10));
+  const querySnapshot = await getDocs(q);
+  const list = [];
+  querySnapshot.docs.forEach((e) =>
+    list.push({
+      id: e.id,
+      ...e.data(),
+    })
+  );
+
+  return list;
 };
